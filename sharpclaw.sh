12cabcce779976b9fe8ed654d06c544287d0494d
Rich Edwards <github@cabbagesofdoom.co.uk>
2025-04-15 22:07:21 +0100

    v0.2.0 : Updated main manager script to run in shell rather than slurm based on $RUNCMD.

--- sharpclaw.sh
+++ sharpclaw.sh
@@ -2,23 +2,24 @@
 ### SHARPCLAW: Synteny, Homology And Repeat Pre-curation ~~~~~ ###
 ###            for Chromosome-Level Assembly Workflows   ~~~~~ ###
 ### MAIN WORKFLOW SHELL SCRIPT                           ~~~~~ ###
-### LAST EDIT: 17/09/24                                  ~~~~~ ###
+### LAST EDIT: 15/04/25                                  ~~~~~ ###
 ### AUTHORS: Richard Edwards 2024                        ~~~~~ ###
 ### CONTACT: rich.edwards@uwa.edu.au                     ~~~~~ ###
 ##################################################################
 
 SCRIPT=sharpclaw.sh
-VERSION="v0.1.0"
+VERSION="v0.2.0"
 
 # This is the primary script for the SHARPCLAW pre-curation workflow
 
 ####################################### ::: HISTORY ::: ############################################
 # v0.1.0 : Initial working version, developed for Ocean Genomes OG820.
+# v0.2.0 : Updated main manager script to run in shell rather than slurm based on $RUNCMD.
 
 ####################################### ::: TO DO ::: ##############################################
-# [ ] : Generate and test full working version.
-# [ ] : Add robust checks for input files and software.
-# [ ] : Consider partitioning stages into subdirectories.
+# [Y] : Generate and test full working version.
+# [Y] : Add robust checks for input files and software.
+# [Y] : Consider partitioning stages into subdirectories.
 # [ ] : Add email to scripts.
 
 ####################################### ::: SETUP ::: #############################################
@@ -42,16 +43,33 @@ echo "#---------------" | tee -a $LOG
 ####################################### ::: WORKFLOW  ::: ##########################################
 #i# SharpClaw currently queues a single workflow that is processed sequentially.
 #i# Once the core workflow is established, it will be partitioned and wrapped more effectively.
-JOB=sh$NEWBASE; 
-cat $SCRIPTDIR/jobhead.sh | sed "s/cpus-per-task=16/cpus-per-task=$NCPU/" | sed "s/mem=28G/mem=${MEM}G/" | \
-	sed "s/job-name=sharpclaw/job-name=$JOB/" | tee $JOB.sh
+JOB=sh$NEWBASE;
+#i# First setup job headers if required
+if [ "$RUNCMD" = "sbatch" ]; then
+  cat $SCRIPTDIR/jobhead.sh | sed "s/cpus-per-task=16/cpus-per-task=$NCPU/" | sed "s/mem=28G/mem=${MEM}G/" | \
+	  sed "s/job-name=sharpclaw/job-name=$JOB/" | tee $JOB.sh
+fi
+if [ "$RUNCMD" = "qsub" ]; then
+  echo "QSub not yet implemented."
+  #cat $SCRIPTDIR/jobhead.sh | sed "s/cpus-per-task=16/cpus-per-task=$NCPU/" | sed "s/mem=28G/mem=${MEM}G/" | \
+	#  sed "s/job-name=sharpclaw/job-name=$JOB/" | tee $JOB.sh
+fi
+if [ "$RUNCMD" = "source" ]; then
+  echo "#!/bin/bash --login" | tee $JOB.sh
+fi
 
+#i# Next, run the main code - set variables with $CONFIG and then execute main script
 cat $CONFIG | tee -a $JOB.sh
 echo | tee -a $JOB.sh
 echo "$SETUPENV" | tee -a $JOB.sh
 cat $EXECDIR/sharpclaw.exec | tee -a $JOB.sh
-cat $SCRIPTDIR/jobtail.sh | tee -a $JOB.sh  
 
+#i# Add any job ending code, if required
+if [ "$RUNCMD" = "sbatch" ]; then
+  cat $SCRIPTDIR/jobtail.sh | tee -a $JOB.sh  
+fi
+
+#i# End script and execute
 echo "[$(date)] Running full SHARPCLAW: $JOB.sh" | tee -a $LOG
 $RUNCMD $JOB.sh
 
